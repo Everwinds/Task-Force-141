0816038673725f16dc0ef67b2524f7ae965e5a67
Howard <howardhuousa@gmail.com>
2021-01-08 16:19:54 +0800

    Pathfinding Test

---
+++ TP Adventure/Assets/Scripts/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int x = 0;
+    public int y = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TP Adventure/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    private List<Node> nodes = new List<Node>();
+
+    void Start()
+    {
+        for(int i=0; i<transform.childCount; i++) nodes.Add(transform.GetChild(i).GetComponent<Node>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
