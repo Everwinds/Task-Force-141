231835ee1fa507b208fb0895cde99da656ff7d88
King Zhou <zhouyimingking@outlook.com>
2021-01-09 17:52:13 +0800

    Player控制脚本分模块 添加了Player的prefab

---
+++ TP Adventure/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(1f,10f)]
+    public float speed = 2f;
+    public int currentLayer = 0;
+
+    Rigidbody2D rb2d;
+    Animator animator;
+    Vector3 moveDir = Vector3.zero;
+    bool movable = false;
+    int directionIndex = 1;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (PauseMenu.Instance.paused)
+        {
+            animator.speed = 0;
+            return;
+        }
+        
+        HandleMovement();
+        PlayerSkills.HandleCliming((Vector2)moveDir, ref currentLayer, this.transform);
+    }
+
+    private void FixedUpdate()
+    {
+        if (PauseMenu.Instance.paused) 
+            return;
+        if (movable)
+            Move();
+    }
+
+    private void Move()
+    {
+        rb2d.velocity = moveDir * speed;
+        animator.speed = 1;
+        int dir = 1;
+        if(moveDir.y>0)
+        {
+            if (moveDir.x > 0) dir = 1;
+            else dir = 2;
+        }
+        else
+        {
+            if (moveDir.x < 0) dir = 3;
+            else dir = 4;
+        }
+        if(directionIndex!=dir)
+        {
+            directionIndex = dir;
+            switch (dir)
+            {
+                case 1:
+                    animator.SetTrigger("UpRight");
+                    break;
+
+                case 2:
+                    animator.SetTrigger("UpLeft");
+                    break;
+
+                case 3:
+                    animator.SetTrigger("DownLeft");
+                    break;
+
+                case 4:
+                    animator.SetTrigger("DownRight");
+                    break;
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 mouseDir = Input.mousePosition-Camera.main.WorldToScreenPoint(transform.position);
+        if (Input.GetMouseButton(0))
+        {
+            if (!movable) movable = true;
+            moveDir = mouseDir.normalized;
+        }
+        else
+        {
+            if (movable) movable = false;
+            //moveDir = Vector3.zero;
+            animator.speed = 0;
+        }
+    }
+}
---
+++ TP Adventure/Assets/Scripts/Gameplay/PlayerSkills.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerSkills
+{
+    public static void HandleCliming(Vector2 moveDirection, ref int currentLayer, Transform transform)
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GoUp(moveDirection, ref currentLayer, transform);
+        }
+    }
+
+    private static void GoUp(Vector2 moveDirection, ref int currentLayer, Transform transform)
+    {
+        int up = 0;
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, moveDirection, .4f);
+        Debug.DrawLine(transform.position, (Vector2)transform.position + moveDirection, Color.blue);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.CompareTag("Handle"))
+            {
+                up = hit.transform.position.y > transform.position.y ? 1 : -1;
+                break;
+            }
+        }
+
+        if (up == 0)
+        return;
+
+        GameObject[] currentLayerWalls = GameObject.FindGameObjectsWithTag(currentLayer.ToString());
+        foreach (GameObject wall in currentLayerWalls)
+        {
+            wall.layer = 10;
+        }
+
+        currentLayer += up;
+
+        GameObject[] currentLayerGrounds = GameObject.FindGameObjectsWithTag(currentLayer.ToString());
+        foreach (GameObject ground in currentLayerGrounds)
+        {
+            ground.layer = 9;
+        }
+
+        //replace with animation
+        transform.position += Vector3.up * 2.5f * up;
+        transform.position += (Vector3)moveDirection / 2;
+    }
+}
