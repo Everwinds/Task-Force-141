6e413cefcd83a4789cd2fce149b53c54b24b69f2
Howard <howardhuousa@gmail.com>
2021-01-09 23:35:29 +0800

    Start Menu

---
+++ TP Adventure/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject background;
+    public GameObject curtain;
+
+    public void GameStart()
+    {
+        StartCoroutine(GameStartCoroutine());
+    }
+
+    public IEnumerator GameStartCoroutine()
+    {
+        LeanTween.alpha(curtain.GetComponent<RectTransform>(), 1f, 1f);
+        yield return new WaitForSeconds(1f);
+        background.SetActive(false);
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        yield return new WaitForSeconds(0.2f);
+        camera.SetActive(false);
+        LeanTween.alpha(curtain.GetComponent<RectTransform>(), 0f, 1f);
+        yield return new WaitForSeconds(1f);
+        SceneManager.UnloadSceneAsync(0);
+    }
+}
---
+++ TP Adventure/Assets/Scripts/TransitionText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionText : MonoBehaviour
+{
+    public string[] transitionTexts;
+    public Text text;
+
+    private CanvasGroup canvasGroup;
+    private bool fading = false;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public IEnumerator FadeIn(int level)
+    {
+        if (fading) yield break;
+        fading = true;
+        canvasGroup.alpha = 0f;
+        text.text = transitionTexts[level - 1];
+        LeanTween.alphaCanvas(canvasGroup, 1f, 1f);
+        yield return new WaitForSeconds(4f);
+        LeanTween.alphaCanvas(canvasGroup, 0f, 1f);
+        yield return new WaitForSeconds(1f);
+        fading = false;
+    }
+}
